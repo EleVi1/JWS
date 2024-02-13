737a376758594870aec4be7bd74fddf4ad75fcae
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-13 14:50:42 +0100

    lastwehope

--- database/src/main/java/fr/epita/assistants/data/model/CourseModel.java
+++ database/src/main/java/fr/epita/assistants/data/model/CourseModel.java
@@ -1,6 +1,7 @@
 package fr.epita.assistants.data.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,9 +12,11 @@ public class CourseModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
+    @Column @Size(max = 255)
     public String name;
     @ElementCollection
     @CollectionTable(name = "course_model_tags",
             joinColumns = @JoinColumn(name = "course_id"))
+    @Column(name = "tag")
     public Set<String> course_tag = new HashSet<>();
 }
