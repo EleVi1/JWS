ff2a1a7d91ed13962395ce08470f007a6cbe08ed
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-13 14:23:11 +0100

    sec

--- database/src/main/java/fr/epita/assistants/data/model/CourseModel.java
+++ database/src/main/java/fr/epita/assistants/data/model/CourseModel.java
@@ -1,8 +1,5 @@
 package fr.epita.assistants.data.model;
 
-import org.hibernate.annotations.Entity;
-import org.hibernate.type.StringNVarcharType;
-
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,18 +7,13 @@ import java.util.Set;
 @Entity
 @Table(name = "course_model")
 public class CourseModel {
-    @Id // primary key
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
-    @Column(name = "name") // default 255
     public String name;
-
-    //@OneToMany(mappedBy = "course")
-    //private List<StudentModel> students = new ArrayList<>();
     @ElementCollection
     @CollectionTable(name = "course_model_tags",
             joinColumns = @JoinColumn(name = "course_id"))
-    @Column(name = "tag")
-    private Set<String> course_tag = new HashSet<>();
+    public Set<String> course_tag = new HashSet<>();
 }
--- database/src/main/java/fr/epita/assistants/data/model/StudentModel.java
+++ database/src/main/java/fr/epita/assistants/data/model/StudentModel.java
@@ -1,21 +1,16 @@
 package fr.epita.assistants.data.model;
 
-import org.hibernate.annotations.Entity;
 import javax.persistence.*;
-import java.math.BigInteger;
 
 @Entity
-@Table(name = "students")
+@Table(name = "student_model")
 public class StudentModel {
     @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) // primary key
-    private Long id;
+    public Long id;
 
-    @Column(name = "name")
-    private String name;
+    public String name;
 
-    @Column(name = "course_id")
-    private Long course_id;
     @ManyToOne
     @JoinColumn(name = "course_id", referencedColumnName = "id")
-    private CourseModel course;
+    public CourseModel course_model;
 }
