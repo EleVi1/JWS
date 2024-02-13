85c899a4e697a42a5953b711123f51719ae8e753
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-13 14:04:26 +0100

    first

--- database/src/main/java/fr/epita/assistants/data/model/StudentModel.java
+++ database/src/main/java/fr/epita/assistants/data/model/StudentModel.java
@@ -1,2 +1,21 @@
-package fr.epita.assistants.data.model;public class StudentModel {
+package fr.epita.assistants.data.model;
+
+import org.hibernate.annotations.Entity;
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "students")
+public class StudentModel {
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) // primary key
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "course_id")
+    private Long course_id;
+    @ManyToOne
+    @JoinColumn(name = "course_id", referencedColumnName = "id")
+    private CourseModel course;
 }
